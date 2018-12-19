b69c5de8cfd57d09e18b466829bb152b929980ad
FedorenkoAl <alexey712003@list.ru>
2018-12-19 15:24:54 +0200

    Правка страницы Глэйси

---
+++ js/script.js
@@ -0,0 +1,12 @@
+ var button = document.querySelector(".address-button");
+ var popup = document.querySelector(".popup");
+ var close = popup.querySelector(".popup button::before");
+
+ button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("popup-clik");
+});
+ close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("popup-clik");
+});
