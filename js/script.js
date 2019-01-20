7b5f2eaba1fd6f16aa03bd5e17a6d60caf608b58
FedorenkoAl <alexey712003@list.ru>
2019-01-20 14:55:49 +0200

    исправления 20.01.2019

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
- var button = document.querySelector(".address-button");
- var popup = document.querySelector(".popup");
- var close = popup.querySelector(".popup button::before");
+var button = document.querySelector(".address-button");
+var popup = document.querySelector(".popup");
+var close = popup.querySelector(".popup button::before");
 
- button.addEventListener("click", function (evt) {
+button.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("popup-clik");
 });
- close.addEventListener("click", function (evt) {
+close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("popup-clik");
 });
+
